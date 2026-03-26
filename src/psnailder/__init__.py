cb1d00732b3d637a00bfc5ea488f97641559fc4a
Pavadol Yamsiri <pavyamsiri@gmail.com>
2026-03-26 19:51:56 +1100

    __init__: Add docstrings

--- src/psnailder/__init__.py
+++ src/psnailder/__init__.py
@@ -1,10 +1,11 @@
+"""A module that implements  the phase spiral fitting algorithm described in Alinder et al. 2023."""
+
 from __future__ import annotations
 
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 import emcee
-from typing import TypedDict
 import numpy as np
 from scipy import ndimage, special, stats
 
@@ -15,24 +16,38 @@ if TYPE_CHECKING:
     from optype import numpy as onp
 
 
-@dataclass
-class ParamBounds:
-    """A class to describe the bounds of a parameter."""
+__all__ = ["SpiralFitter"]
 
-    name: ParamName
+
+@dataclass
+class _ParamBounds:
+    """A class to describe the bounds of a parameter.
+
+    Attributes
+    ----------
+    name : ParamName
+        The name of the parameter.
+    lo : float
+        The lower bound.
+    hi : float
+        The higher bound.
+
+    """
+
+    name: _ParamName
     lo: float
     hi: float
 
 
-DEFAULT_PARAM_BOUNDS: Final[tuple[ParamBounds, ...]] = (
-    ParamBounds(name="alpha", lo=0.0, hi=1.0),
-    ParamBounds(name="b", lo=0.005, hi=0.1),
-    ParamBounds(name="c", lo=0.0, hi=0.004),
-    ParamBounds(name="theta0", lo=-np.pi, hi=np.pi),
-    ParamBounds(name="scale_factor", lo=30.0, hi=70.0),
-    ParamBounds(name="rho", lo=0.0, hi=0.18),
+_DEFAULT_PARAM_BOUNDS: Final[tuple[_ParamBounds, ...]] = (
+    _ParamBounds(name="alpha", lo=0.0, hi=1.0),
+    _ParamBounds(name="b", lo=0.005, hi=0.1),
+    _ParamBounds(name="c", lo=0.0, hi=0.004),
+    _ParamBounds(name="theta0", lo=-np.pi, hi=np.pi),
+    _ParamBounds(name="scale_factor", lo=30.0, hi=70.0),
+    _ParamBounds(name="rho", lo=0.0, hi=0.18),
 )
-NUM_PARAMETERS: Final[int] = 6
+_NUM_PARAMETERS: Final[int] = 6
 _ALPHA_INDEX: Final[int] = 0
 _B_INDEX: Final[int] = 1
 _C_INDEX: Final[int] = 2
@@ -40,12 +55,29 @@ _THETA0_INDEX: Final[int] = 3
 _SCALE_FACTOR_INDEX: Final[int] = 4
 _RHO_INDEX: Final[int] = 5
 
-type SmoothingFunc = Callable[[onp.Array2D[np.float64]], onp.Array2D[np.float64]]
-type ParamName = Literal["alpha", "b", "c", "theta0", "scale_factor", "rho"]
+type _SmoothingFunc = Callable[[onp.Array2D[np.float64]], onp.Array2D[np.float64]]
+type _ParamName = Literal["alpha", "b", "c", "theta0", "scale_factor", "rho"]
 
 
 @dataclass
 class SpiralFitDiagnostics:
+    """A result of the spiral fitting process.
+
+    Attributes
+    ----------
+    initial_model : AlinderModel
+        The initial model.
+    final_model : AlinderModel
+        The best fit model.
+    num_iterations : int
+        The number of iterations taken.
+    max_iterations : int | None
+        The maximum number of iterations.
+    converged : bool
+        A flag signifying whether fitting converged before the fitting process stopped.
+
+    """
+
     initial_model: AlinderModel
     final_model: AlinderModel
     num_iterations: int
@@ -55,15 +87,51 @@ class SpiralFitDiagnostics:
 
 @dataclass
 class AlinderModel:
+    """A Alinder et al. 2023 phase spiral model.
+
+    Attributes
+    ----------
+    alpha : float
+        The spiral amplitude.
+    b : float
+        The linear winding amplitude.
+    c : float
+        The quadratic winding amplitude.
+    theta0 : float
+        The angle offset in radians.
+    scale_factor : float
+        The scale factor.
+    rho : float
+        The flattening function distance.
+    background : Array2D[f64]
+        The background.
+
+    """
+
+    alpha: float
     b: float
     c: float
-    alpha: float
     theta0: float
     scale_factor: float
     rho: float
     background: onp.Array2D[np.float64]
 
     def perturbation(self, z_mesh: onp.Array2D[np.float64], vz_mesh: onp.Array2D[np.float64]) -> onp.Array2D[np.float64]:
+        """Calculate the perturbation.
+
+        Parameters
+        ----------
+        z_mesh : Array2D[f64]
+            The z values for each cell.
+        vz_mesh : Array2D[f64]
+            The Vz values for each cell.
+
+        Returns
+        -------
+        perturbation : Array2D[f64]
+            The perturbation.
+
+        """
         r_mesh = np.hypot(z_mesh, vz_mesh / self.scale_factor)
         theta_mesh = np.arctan2(vz_mesh, self.scale_factor * z_mesh)
 
@@ -76,47 +144,73 @@ class AlinderModel:
         return 1.0 + self.alpha * flattening * np.cos(theta_mesh - phase - self.theta0)
 
     def phase_angle(self) -> float:
-        r_test: Final[float] = 0.5
+        """Calculate the phase angle.
+
+        Returns
+        -------
+        phase_angle : float
+            The phase angle in radians.
+
+        """
+        R_TEST: Final[float] = 0.5
 
         b_over_2c = 0.5 * self.b / self.c
-        phase = -b_over_2c + np.sqrt(np.square(b_over_2c) + r_test / self.c) if self.c != 0.0 else r_test / self.b
+        phase = -b_over_2c + np.sqrt(np.square(b_over_2c) + R_TEST / self.c) if self.c != 0.0 else R_TEST / self.b
         return phase + self.theta0
 
 
 @dataclass
 class AlinderModelCollection:
+    """A collection of Alinder et al. 2023 phase spiral models.
+
+    Attributes
+    ----------
+    parameters : Array2D[f64]
+        The model parameters in the shape (num_walkers, 6).
+    background : Array2D[f64]
+        The background.
+
+    """
+
     parameters: onp.Array2D[np.float64]
     background: onp.Array2D[np.float64]
 
     def __post_init__(self) -> None:
-        assert self.parameters.shape[1] == NUM_PARAMETERS
+        assert self.parameters.shape[1] == _NUM_PARAMETERS
 
     @property
     def num_walkers(self) -> int:
+        """int: The number of walkers."""
         return self.parameters.shape[0]
 
     @property
     def alpha(self) -> onp.Array1D[np.float64]:
+        """float: The spiral amplitude."""
         return self.parameters[:, _ALPHA_INDEX]
 
     @property
     def b(self) -> onp.Array1D[np.float64]:
+        """float: The linear winding amplitude."""
         return self.parameters[:, _B_INDEX]
 
     @property
     def c(self) -> onp.Array1D[np.float64]:
+        """float: The quadratic winding amplitude."""
         return self.parameters[:, _C_INDEX]
 
     @property
     def theta0(self) -> onp.Array1D[np.float64]:
+        """float: The angle offset in radians."""
         return self.parameters[:, _THETA0_INDEX]
 
     @property
     def scale_factor(self) -> onp.Array1D[np.float64]:
+        """float: The scale factor."""
         return self.parameters[:, _SCALE_FACTOR_INDEX]
 
     @property
     def rho(self) -> onp.Array1D[np.float64]:
+        """float: The flattening function distance."""
         return self.parameters[:, _RHO_INDEX]
 
     def perturbation(
@@ -125,6 +219,23 @@ class AlinderModelCollection:
         vz_mesh: onp.Array2D[np.float64],
         valid: onp.Array1D[np.bool_],
     ) -> onp.Array3D[np.float64]:
+        """Calculate the perturbation.
+
+        Parameters
+        ----------
+        z_mesh : Array2D[f64]
+            The z values for each cell.
+        vz_mesh : Array2D[f64]
+            The Vz values for each cell.
+        valid : Array2D[bool]
+            A mask over the valid parameters.
+
+        Returns
+        -------
+        perturbation : Array2D[f64]
+            The perturbation.
+
+        """
         alpha_arr = self.alpha
         b_arr = self.b
         c_arr = self.c
@@ -152,6 +263,8 @@ class AlinderModelCollection:
 
 
 class SpiralFitter:
+    """A configuration of the Alinder et al 2023 fitting algorithm."""
+
     def __init__(
         self,
         *,
@@ -159,30 +272,52 @@ class SpiralFitter:
         num_discard: int = 1_000,
         num_walkers: int = 32,
         max_iterations: int | None = 50,
-        smoothing_func: SmoothingFunc | None = None,
-        param_lo: dict[ParamName, float] | None = None,
-        param_hi: dict[ParamName, float] | None = None,
+        smoothing_func: _SmoothingFunc | None = None,
+        param_lo: dict[_ParamName, float] | None = None,
+        param_hi: dict[_ParamName, float] | None = None,
     ) -> None:
+        """Construct a fitter.
+
+        Parameters
+        ----------
+        num_samples : int
+            The number of MCMC posterior samples.
+        num_discard : int
+            The number of MCMC samples to discard/burn in.
+        num_walkers : int
+            The number of walkers.
+        max_iterations : int | None
+            The maximum number of background fitting iterations. Set this to `None` to allow
+            not bound the number of iterations.
+        smoothing_func : _SmoothingFunc | None
+            The smoothing function to apply to obtain the background in each iteration. Set this to `None` to
+            use the default smoothing.
+        param_lo : dict[ParamName, float] | None
+            The lower bounds of the parameters. Set this to `None` to use the default bounds.
+        param_hi : dict[ParamName, float] | None
+            The upper bounds of the parameters. Set this to `None` to use the default bounds.
+
+        """
         self._num_samples: int = num_samples
         self._num_discard: int = num_discard
         self._num_walkers: int = num_walkers
         self._max_iterations: int | None = max_iterations
-        self._smoothing_func: SmoothingFunc = SpiralFitter._default_smoothing if smoothing_func is None else smoothing_func
-        self._param_lo: onp.Array1D[np.float64] = np.array([param.lo for param in DEFAULT_PARAM_BOUNDS], dtype=np.float64)
-        self._param_hi: onp.Array1D[np.float64] = np.array([param.hi for param in DEFAULT_PARAM_BOUNDS], dtype=np.float64)
+        self._smoothing_func: _SmoothingFunc = SpiralFitter._default_smoothing if smoothing_func is None else smoothing_func
+        self._param_lo: onp.Array1D[np.float64] = np.array([param.lo for param in _DEFAULT_PARAM_BOUNDS], dtype=np.float64)
+        self._param_hi: onp.Array1D[np.float64] = np.array([param.hi for param in _DEFAULT_PARAM_BOUNDS], dtype=np.float64)
 
-        default_lo: dict[ParamName, float] = {p.name: p.lo for p in DEFAULT_PARAM_BOUNDS}
-        default_hi: dict[ParamName, float] = {p.name: p.hi for p in DEFAULT_PARAM_BOUNDS}
+        default_lo: dict[_ParamName, float] = {p.name: p.lo for p in _DEFAULT_PARAM_BOUNDS}
+        default_hi: dict[_ParamName, float] = {p.name: p.hi for p in _DEFAULT_PARAM_BOUNDS}
 
         if param_lo is not None:
             if unknown := param_lo.keys() - default_lo.keys():
-                msg = f"Unknown parameter names in param_lo: {unknown}"
+                msg = f"Unknown parameter names in `param_lo`: {unknown}"
                 raise ValueError(msg)
             default_lo.update(param_lo)
 
         if param_hi is not None:
             if unknown := param_hi.keys() - default_hi.keys():
-                msg = f"Unknown parameter names in param_hi: {unknown}"
+                msg = f"Unknown parameter names in `param_hi`: {unknown}"
                 raise ValueError(msg)
             default_hi.update(param_hi)
 
@@ -193,11 +328,24 @@ class SpiralFitter:
                 raise ValueError(msg)
 
         # Preserve canonical ordering
-        self._param_lo = np.array([default_lo[p.name] for p in DEFAULT_PARAM_BOUNDS], dtype=np.float64)
-        self._param_hi = np.array([default_hi[p.name] for p in DEFAULT_PARAM_BOUNDS], dtype=np.float64)
+        self._param_lo = np.array([default_lo[p.name] for p in _DEFAULT_PARAM_BOUNDS], dtype=np.float64)
+        self._param_hi = np.array([default_hi[p.name] for p in _DEFAULT_PARAM_BOUNDS], dtype=np.float64)
 
     @staticmethod
     def _default_smoothing(arr: onp.Array2D[np.float64]) -> onp.Array2D[np.float64]:
+        """The default smoothing function to get the background.
+
+        Parameters
+        ----------
+        arr : Array2D[f64]
+            The array to smooth.
+
+        Returns
+        -------
+        smoothed : Array2D[f64]
+            The smoothed array.
+
+        """
         return ndimage.gaussian_filter(arr, sigma=2)
 
     def _ln_prior(self, models: AlinderModelCollection) -> onp.Array1D[np.float64]:
@@ -216,7 +364,7 @@ class SpiralFitter:
         """
         in_bounds: onp.Array1D[np.bool_] = np.ones(models.num_walkers, dtype=np.bool_)
 
-        for index in range(NUM_PARAMETERS):
+        for index in range(_NUM_PARAMETERS):
             in_bounds &= np.logical_and(
                 models.parameters[:, index] >= self._param_lo[index], models.parameters[:, index] <= self._param_hi[index]
             )
@@ -238,8 +386,8 @@ class SpiralFitter:
 
         Parameters
         ----------
-        parameters : Array2D[f64]
-            Parameter vector [alpha, b, c, theta0, S, rho], in the shape (num_walkers, 6).
+        models : AlinderModelVectorised
+            A collection of spiral models.
         counts : Array2D[f64]
             The observed data in a grid.
         background : Array2D[f64]
@@ -283,6 +431,27 @@ class SpiralFitter:
         vz_bins: onp.Array1D[np.float64],
         seed: int | None = None,
     ) -> SpiralFitDiagnostics:
+        """Fit a phase spiral to the given vertical phase space distribution.
+
+        Parameters
+        ----------
+        z : Array1D[f64]
+            The z coordinate of the stars.
+        vz : Array1D[f64]
+            The Vz velocity of the stars.
+        z_bins : Array1D[f64]
+            The bin edges over z.
+        vz_bins : Array1D[f64]
+            The bin edges over vz.
+        seed : int | None
+            The random seed to use or `None` if no seed.
+
+        Returns
+        -------
+        result : SpiralFitDiagnostics
+            The fitting result.
+
+        """
         z_centres = 0.5 * (z_bins[:-1] + z_bins[1:])
         vz_centres = 0.5 * (vz_bins[:-1] + vz_bins[1:])
         vz_mesh, z_mesh = np.meshgrid(vz_centres, z_centres)
@@ -311,6 +480,27 @@ class SpiralFitter:
         vz_mesh: onp.Array2D[np.float64],
         seed: int | None = None,
     ) -> SpiralFitDiagnostics:
+        """Fit a phase spiral to the given vertical phase space map and background.
+
+        Parameters
+        ----------
+        initial_density : Array2D[f64]
+            The initial density.
+        initial_background : Array2D[f64]
+            The initial background.
+        z_mesh : Array2D[f64]
+            The z values for each cell.
+        vz_mesh : Array2D[f64]
+            The Vz values for each cell.
+        seed : int | None
+            The random seed to use or `None` if no seed.
+
+        Returns
+        -------
+        result : SpiralFitDiagnostics
+            The fitting result.
+
+        """
         rng = np.random.default_rng(seed=seed)
         np.random.seed(seed)
         mask = _mask(z_mesh, vz_mesh)
@@ -325,13 +515,13 @@ class SpiralFitter:
             num_iterations += 1
             sampler = emcee.EnsembleSampler(
                 self._num_walkers,
-                NUM_PARAMETERS,
+                _NUM_PARAMETERS,
                 ln_prob,
                 args=(self, initial_density, background, z_mesh, vz_mesh),
                 vectorize=True,
             )
 
-            p0 = rng.uniform(self._param_lo, self._param_hi, size=(self._num_walkers, NUM_PARAMETERS))
+            p0 = rng.uniform(self._param_lo, self._param_hi, size=(self._num_walkers, _NUM_PARAMETERS))
             sampler.run_mcmc(p0, self._num_samples, progress=False)
 
             flat_samples = sampler.get_chain(discard=self._num_discard, flat=True)
@@ -380,10 +570,42 @@ class SpiralFitter:
 
 
 def _mask(z_mesh: onp.Array2D[np.float64], vz_mesh: onp.Array2D[np.float64]) -> onp.Array2D[np.float64]:
+    """Construct a mask used when evaluating quality.
+
+    Parameters
+    ----------
+    z_mesh : Array2D[f64]
+        The z values for each cell.
+    vz_mesh : Array2D[f64]
+        The Vz values for each cell.
+
+    Returns
+    -------
+    mask : Array2D[f64]
+        The mask.
+
+    """
     return -special.expit(np.square(z_mesh) + np.square(vz_mesh / 40) - 1.0) + 1.0
 
 
 def _calculate_rmse(data: onp.Array2D[np.float64], estimate: onp.Array2D[np.float64], mask: onp.Array2D[np.float64]) -> float:
+    """Calculate the root mean sum of square residuals.
+
+    Parameters
+    ----------
+    data : Array2D[f64]
+        The data to compare to.
+    estimate : Array2D[f64]
+        The estimate/prediction.
+    mask : Array2D[f64]
+        The mask.
+
+    Returns
+    -------
+    rmse : float
+        The root mean sum of square residuals.
+
+    """
     return np.sqrt(np.mean(np.square(mask * (data - estimate))))
 
 
@@ -401,14 +623,16 @@ def ln_prob(
     ----------
     parameters : Array2D[f64]
         Parameter vector [alpha, b, c, theta0, S, rho], in the shape (num_walkers, 6).
+    fitter : SpiralFitter
+        The fitting configuration.
     counts : Array2D[f64]
         The observed data in a grid.
     background : Array2D[f64]
         The background.
     z_mesh : Array2D[f64]
-        The z value at each mesh point.
+        The z values for each cell.
     vz_mesh : Array2D[f64]
-        The vz value at each mesh point.
+        The Vz values for each cell.
 
     Returns
     -------
