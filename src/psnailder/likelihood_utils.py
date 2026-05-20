d87008aa4c567902b05d301fcd70cb52256b7a1d
Pavadol Yamsiri <pavyamsiri@gmail.com>
2026-05-20 13:01:24 +1000

    model: Add pvalue

--- src/psnailder/likelihood_utils.py
+++ src/psnailder/likelihood_utils.py
@@ -1,9 +1,11 @@
 """Module containing functions to compute the likelihoods used."""
 
 from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
 import numpy as np
+from scipy import stats
 
 if TYPE_CHECKING:
     from optype import numpy as onp
@@ -36,3 +38,44 @@ def ln_likelihood(data: onp.Array2D[np.float64], prediction: onp.Array2D[np.floa
     valid = denom > 0
 
     return -0.5 * np.sum(square_residuals[valid] / denom[valid])
+
+
+def lrt_pvalue(
+    data: onp.Array2D[np.float64],
+    alt: onp.Array2D[np.float64],
+    null: onp.Array2D[np.float64],
+    mask: onp.Array2D[np.float64],
+    *,
+    dof: int,
+) -> float:
+    """Calculate the likelihood ratio with respect to the null model and return its p-value.
+
+    Parameters
+    ----------
+    data : Array2D[f64]
+        The observed data.
+    alt : Array2D[f64]
+        The alternative model prediction.
+    null : Array2D[f64]
+        The null model prediction.
+    mask : Array2D[f64]
+        The mask to de-emphasise the less important parts of the phase plane.
+
+    Returns
+    -------
+    pvalue : float
+        The p-value representing the probability that the null model is likely over the alternative model.
+
+    """
+    ln_like_alt = ln_likelihood(
+        data,
+        alt,
+        mask,
+    )
+    ln_like_null = ln_likelihood(
+        data,
+        null,
+        mask,
+    )
+    lmbd = -2.0 * (ln_like_null - ln_like_alt)
+    return stats.chi2.sf(lmbd, df=dof)
--- src/psnailder/model.py
+++ src/psnailder/model.py
@@ -1,12 +1,14 @@
 """Module containing the phase spiral data model based off Alinder et. al. 2023 and Alinder et. al. 2024."""
 
 from __future__ import annotations
+
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 import numpy as np
 
 from .component import PSpiralComponent
+from .likelihood_utils import lrt_pvalue
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
@@ -39,3 +41,11 @@ class PSpiralModel:
         signal[~np.isfinite(signal)] = 1.0
 
         return signal
+
+    def pvalue(self, data: onp.Array2D[np.float64], mask: onp.Array2D[np.float64]) -> float:
+        assert self.z_mesh.ndim == 2
+        assert self.z_mesh.shape == self.vz_mesh.shape
+        assert self.z_mesh.shape == self.background.shape
+        assert self.z_mesh.shape == data.shape
+
+        return lrt_pvalue(data, self.prediction(), self.background, mask, dof=6)
