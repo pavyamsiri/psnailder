0a192331fcf7e5aeedb12e60dc559dc5f53afb56
Pavadol Yamsiri <pavyamsiri@gmail.com>
2026-05-20 11:56:55 +1000

    psnailder: Create a new component class and model class
    Preparing for the multi-arm scenario.

---
+++ src/psnailder/component.py
@@ -0,0 +1,157 @@
+"""Module containing the data representation of a phase spiral arm component."""
+
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Any
+
+from scipy import special
+import numpy as np
+
+from ._shape_utils import verify_array_shape
+
+if TYPE_CHECKING:
+    from optype import numpy as onp
+
+
+@dataclass
+class PSpiralComponent:
+    """A phase spiral component.
+
+    Attributes
+    ----------
+    alpha : float
+        The spiral amplitude.
+    b : float
+        The linear winding amplitude.
+    c : float
+        The quadratic winding amplitude.
+    theta0 : float
+        The angle offset in radians.
+    scale_factor : float
+        The scale factor.
+    rho : float
+        The flattening function distance.
+
+    """
+
+    alpha: float
+    b: float
+    c: float
+    theta0: float
+    scale_factor: float
+    rho: float
+    flattening_strength: float = 0.1
+
+    def perturbation[ShapeT: tuple[Any, ...]](
+        self, z: onp.ArrayND[np.float64, ShapeT], vz: onp.ArrayND[np.float64, ShapeT]
+    ) -> onp.ArrayND[np.float64, ShapeT]:
+        """Calculate the contribution to the perturbation from this component.
+
+        The form being f(r, theta) = 1 + alpha * flattening(r, rho) * cos(theta - phi_s(r) - theta0).
+
+        Parameters
+        ----------
+        z_mesh : ArrayND[f64, S]
+            The z coordinates.
+        vz_mesh : ArrayND[f64, S]
+            The vz coordinates.
+
+        Returns
+        -------
+        perturbation : ArrayND[f64, S]
+            The perturbation.
+
+        """
+        assert z.shape == vz.shape
+
+        scaled_z = np.multiply(z, self.scale_factor)
+        scaled_vz = vz * np.reciprocal(self.scale_factor)
+        r_mesh = np.hypot(z, scaled_vz)
+        theta_mesh = np.arctan2(vz, scaled_z)
+
+        phase = self.spiral_phase(r_mesh)
+
+        flattening = special.expit((r_mesh - self.rho) / self.flattening_strength)
+        pert = 1.0 + self.alpha * flattening * np.cos(theta_mesh - phase - self.theta0)
+        return verify_array_shape(pert, z.shape)
+
+    def spiral_phase[ShapeT: tuple[Any, ...]](self, r: onp.ArrayND[np.float64, ShapeT]) -> onp.ArrayND[np.float64, ShapeT]:
+        """Compute the phase angle of the spiral phi_s(r).
+
+        Parameters
+        ----------
+        r : ArrayND[f64, S]
+            The phase distance.
+
+        Returns
+        -------
+        phase : ArrayND[f64, S]
+            The spiral phase in radians.
+
+        """
+        sign: np.float64 = np.sign(self.b) if self.b != 0.0 else np.float64(1.0)
+        abs_b: np.float64 = np.abs(self.b).astype(np.float64)
+        abs_c: np.float64 = np.abs(self.c)
+        # phi_s(r) = (+/-) (-b/2c + sqrt((b/2c)^2 + r/c))
+        if abs_c != 0.0:
+            half_b_over_c = 0.5 * abs_b / abs_c
+            phase = sign * (-half_b_over_c + np.sqrt(np.square(half_b_over_c) + r / abs_c))
+        # phi_s(r) = (+/-) r / b
+        else:
+            phase = sign * (r / abs_b)
+        return verify_array_shape(phase, r.shape)
+
+    def model_phase(self, r_test: float = 0.5) -> float:
+        """Calculate the model phase angle.
+
+        Parameters
+        ----------
+        r_test : float
+            The reference phase distance to calculate the angle at.
+
+        Returns
+        -------
+        model_phase : float
+            The model phase angle in radians.
+
+        """
+        phase = float(self.spiral_phase(np.array(r_test))[0])
+        return phase + self.theta0
+
+    @staticmethod
+    def from_array(parameters: onp.Array1D[np.float64]) -> PSpiralComponent:
+        """Convert an array of parameters into a spiral component.
+
+        Parameters
+        ----------
+        parameters : Array1D[f64]
+            The parameters array in the form [alpha, b, c, theta0, scale_factor, rho].
+
+        Returns
+        -------
+        SpiralComponent
+            The spiral component.
+
+        """
+        assert parameters.ndim == 1
+        assert len(parameters) == 6
+
+        return PSpiralComponent(
+            alpha=parameters[0],
+            b=parameters[1],
+            c=parameters[2],
+            theta0=parameters[3],
+            scale_factor=parameters[4],
+            rho=parameters[5],
+        )
+
+    def to_array(self) -> onp.Array1D[np.float64]:
+        """Return the parameters as an array.
+
+        Returns
+        -------
+        parameters : Array1D[f64]
+            The parameters array in the form [alpha, b, c, theta0, scale_factor, rho].
+
+        """
+        return np.array([self.alpha, self.b, self.c, self.theta0, self.scale_factor, self.rho], dtype=np.float64)
---
+++ src/psnailder/model.py
@@ -0,0 +1,41 @@
+"""Module containing the phase spiral data model based off Alinder et. al. 2023 and Alinder et. al. 2024."""
+
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+import numpy as np
+
+from .component import PSpiralComponent
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    from optype import numpy as onp
+
+
+@dataclass
+class PSpiralModel:
+    components: Sequence[PSpiralComponent]
+    z_mesh: onp.Array2D[np.float64]
+    vz_mesh: onp.Array2D[np.float64]
+    background: onp.Array2D[np.float64]
+
+    def prediction(self) -> onp.Array2D[np.float64]:
+        assert self.z_mesh.ndim == 2
+        assert self.z_mesh.shape == self.vz_mesh.shape
+        assert self.z_mesh.shape == self.background.shape
+        return self.background * self.signal()
+
+    def signal(self) -> onp.Array2D[np.float64]:
+        assert self.z_mesh.ndim == 2
+        assert self.z_mesh.shape == self.vz_mesh.shape
+        assert self.z_mesh.shape == self.background.shape
+
+        signal: onp.Array2D[np.float64] = np.full_like(self.z_mesh, -np.inf, dtype=np.float64)
+
+        for comp in self.components:
+            signal = np.maximum(signal, comp.perturbation(self.z_mesh, self.vz_mesh))
+        signal[~np.isfinite(signal)] = 1.0
+
+        return signal
