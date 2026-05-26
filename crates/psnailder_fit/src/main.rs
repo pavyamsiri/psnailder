ebdfaf677dc8f88e453c3e7634053b2d565e9d3a
Pavadol Yamsiri <pavyamsiri@gmail.com>
2026-05-27 02:48:25 +1000

    fit: Playing around with the mock data
    To test the fitting algorithm

--- crates/psnailder_fit/src/main.rs
+++ crates/psnailder_fit/src/main.rs
@@ -9,16 +9,28 @@ fn linspace(start: f64, end: f64, n: usize) -> Vec<f64> {
 
 fn main() {
     let model = MockModel::new(
-        vec![SignalComponent::LogSpiral(PSpiralComponent {
-            alpha: 0.5,
-            b: 0.05,
-            c: 0.002,
-            theta0: 0.0,
-            scale_factor: 40.0,
-            rho: 0.09,
-            winding: -1,
-            flattening_strength: 0.1,
-        })],
+        vec![
+            SignalComponent::LogSpiral(PSpiralComponent {
+                alpha: 0.5,
+                b: 0.05,
+                c: 0.002,
+                theta0: -core::f64::consts::FRAC_PI_2,
+                scale_factor: 40.0,
+                rho: 0.09,
+                winding: -1,
+                flattening_strength: 0.1,
+            }),
+            SignalComponent::LogSpiral(PSpiralComponent {
+                alpha: 0.5,
+                b: 0.05,
+                c: 0.002,
+                theta0: core::f64::consts::FRAC_PI_2,
+                scale_factor: 40.0,
+                rho: 0.09,
+                winding: -1,
+                flattening_strength: 0.1,
+            }),
+        ],
         vec![BackgroundComponent::Gaussian(GaussianComponent {
             x_scale: 1.0,
             y_scale: 40.0,
@@ -34,8 +46,8 @@ fn main() {
     let y_edges = linspace(-60.0, 60.0, num_y_bins + 1);
     let mock_result = model.mock_grid(&x_edges, &y_edges, 1_000_000);
 
-    let tiktak1d = psnailder_tiktak::TikTak::<6>::new(10, 128.0f32.recip(), 0.1, 0.995);
-    let tiktak2d = psnailder_tiktak::TikTak::<12>::new(10, 128.0f32.recip(), 0.1, 0.995);
+    let tiktak1d = psnailder_tiktak::TikTak::<6>::new(12, 128.0f32.recip(), 0.1, 0.995);
+    let tiktak2d = psnailder_tiktak::TikTak::<12>::new(12, 128.0f32.recip(), 0.1, 0.995);
     let fitter = PSpiralFitter {
         fitter1d: PSpiralFitterND {
             tiktak: tiktak1d,
@@ -64,32 +76,10 @@ fn main() {
         .map(|(x, y)| -psnailder_core::expit(x * x + (y * y) / (40.0 * 40.0) - 1.0) + 1.0)
         .collect();
 
-    let (density, background, filtered_mask, mesh_x, mesh_y) = {
-        let mut d = vec![];
-        let mut b = vec![];
-        let mut m = vec![];
-        let mut x = vec![];
-        let mut y = vec![];
-        for ((((&di, &bi), &mi), &xi), &yi) in mock_result
-            .density
-            .iter()
-            .zip(&mock_result.background)
-            .zip(&mask)
-            .zip(&mock_result.mesh_x)
-            .zip(&mock_result.mesh_y)
-            .filter(|((((_, _), mi), _), _)| **mi >= 0.5)
-        {
-            d.push(di);
-            b.push(bi);
-            m.push(mi);
-            x.push(xi);
-            y.push(yi);
-        }
-        (d, b, m, x, y)
-    };
+    let density = mock_result.density;
+    let background = mock_result.background;
+    let mesh_x = mock_result.mesh_x;
+    let mesh_y = mock_result.mesh_y;
 
-    println!("density length = {}", mock_result.density.len());
-    println!("filtered density length = {}", density.len());
-
-    fitter.fit_spiral_with_background(&density, &background, &filtered_mask, &mesh_x, &mesh_y);
+    fitter.fit_spiral_with_background(&density, &background, &mask, &mesh_x, &mesh_y);
 }
