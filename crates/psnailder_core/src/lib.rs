0de0f9f30e82058af66eb6b84cfe751add20eab8
Pavadol Yamsiri <pavyamsiri@gmail.com>
2026-06-03 19:03:42 +1000

    core: Fix the mask function
    The mask was actually backwards where the outskirts
    were weighted more than the inner regions.

--- crates/psnailder_core/src/lib.rs
+++ crates/psnailder_core/src/lib.rs
@@ -1,10 +1,11 @@
 use itertools::izip;
 
+/// Create a sigmoid mask function
 pub fn create_sigmoid_mask(x_scale: f64, y_scale: f64) -> impl Fn(f64, f64) -> f64 {
     move |x: f64, y: f64| {
         let xs = x / x_scale;
         let ys = y / y_scale;
-        expit(-(xs * xs + ys * ys - 1.0))
+        -expit(-(xs.mul_add(xs, ys * ys) - 1.0)) + 1.0
     }
 }
 
